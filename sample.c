d522e25c5bb2d4fc91ca173f239bf9c2c360113f
Adam Hultman <adamhultman@uvic.ca>
2019-09-29 17:06:32 -0700

    User, host, cwd print done.

--- sample.c
+++ sample.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int
-main()
-{
-	const char* prompt = "shell> ";
+
+
+
+
+int main(){
+	
+	
+	char prompt[1000];
+	char cwd[256];
+	char host[256];
+	char* user;
+	host[255] = "\0";
+	cwd[255] = "\0";
+	
+	user = getlogin();	
+	gethostname(host, sizeof(host));
+	getcwd(cwd, sizeof(cwd));	
+	
+	strcat(prompt, "SSI: ");
+	strcat(prompt, user);
+	strcat(prompt, "@");
+	strcat(prompt, host);
+	strcat(prompt, ": ");
+	strcat(prompt, cwd);
+	
+	//SSI: %s@%s: %s ", user, host, cwd
 
 	int bailout = 0;
 	while (!bailout) {
@@ -16,8 +41,12 @@ main()
 
 		if (!strcmp(reply, "bye")) {
 			bailout = 1;
-		} else {
+		} 
+		else if (!strcmp(reply, "ls")) {
 			printf("\nYou said: %s\n\n", reply);
+		}
+		else{
+			printf("Command not found");
 		}
 	
 		free(reply);
