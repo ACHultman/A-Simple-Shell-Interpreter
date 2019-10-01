e3bb5d66fab530c923ad021600e970eb7755e76e
Adam Hultman <adamhultman@e-red.csc.uvic.ca>
2019-09-30 18:50:43 -0700

    Background partially implemented. Seg Faults in process_bg, maybe line 120

--- sample.c
+++ sample.c
@@ -11,78 +11,7 @@ struct bg_pro{
     char command[1024];
     struct bg_pro* next;
 };
-
-void process_cd(char *args[10]);
-
-void process_bg(char *args[10]);
-
-void process_bglist(char *args[10]);
-
-char * create_prompt(char prompt[1000]);
-
-void execute_program(char *args[10]){
-    char *cmd = args[0];
-    pid_t pid;
-    int status;
-
-    if((pid = fork()) < 0){
-        printf("ERROR: Forked failed.\n\n");
-        exit(1);
-    }
-    else if (pid == 0){
-        if((execvp(cmd, args) < 0)){
-            printf("ERROR: Execvp failed.\n\n");
-        }
-    }
-    else{
-        while (wait(&status) != pid)
-            ;
-    }
-
-}
-
-int main() {
-    int bailout = 0;
-    while (!bailout) {
-        char prompt[1000] = "";
-
-        char *reply = readline(create_prompt(prompt));
-
-        char *argsv[10];
-        int i = 0;
-        char *token = strtok(reply, " ");
-        if (token == NULL){
-            continue;
-        }
-        for (i = 0; token != NULL; i++) {
-            argsv[i] = token;
-            token = strtok(NULL, " ");
-        }
-        argsv[i] = NULL;
-        int end_of_argsv = i;
-        printf("----------------------------------------\n");
-        for (i = 0; argsv[i] != NULL; ++i)
-            printf("%s\n", argsv[i]);
-
-        /* Note that readline strips away the final \n */
-        /* For Perl junkies, readline automatically chomps the line read */
-        if (!strcmp(argsv[0], "exit")) {
-            bailout = 1;
-        } else if (!strcmp(argsv[0], "cd")) {
-            process_cd(argsv);
-        } else if (!strcmp(argsv[0], "bg")) {
-            process_bg(argsv);
-        } else if (!strcmp(argsv[0], "bglist")) {
-            process_bglist(argsv);
-        } else {
-            execute_program(argsv);
-        }
-
-
-            free(reply);
-    }
-        printf("Auf Wiedersehen!\n");
-}
+struct bg_pro* head = NULL;
 
 char* create_prompt(char prompt[]){
     char cwd[256];
@@ -106,12 +35,24 @@ char* create_prompt(char prompt[]){
     return prompt;
 }
 
-void process_bglist(char *args[10]) {
-
+void bg_list_append(pid_t p, char **args){
+    struct bg_pro *current = head;
+    while(current->next != NULL){
+        current = current->next;
+    }
+    current->pid = p;
+    strcpy(current->command, *args);
+    current->next = NULL;
 }
 
-void process_bg(char *args[10]) {
 
+void print_bglist() {
+    struct bg_pro* current = head;
+    while(current != NULL){
+        printf("%d: ", current->pid);
+        printf("%s ", current->command);
+        current = current -> next;
+    }
 }
 
 
@@ -125,3 +66,131 @@ void process_cd(char *args[10]) {
     chdir(args[1]);
 
 }
+
+void execute_program(char* cmd, char *args[10]){
+    pid_t pid;
+    int status;
+
+    if((pid = fork()) < 0){
+        printf("ERROR: Forked failed.\n\n");
+        exit(1);
+    }
+    else if (pid == 0){
+        if((execvp(cmd, args) < 0)){
+            printf("ERROR: Execvp failed.\n\n");
+        }
+    }
+    else{
+        while(wait(&status) == 0)
+            ;
+    }
+
+}
+
+void execute_bg_program(char* cmd, char *args[9]){
+    printf("%s\n", cmd);
+    printf("In execute_bg_program\n");
+
+    pid_t p;
+    int status;
+
+    if((p = fork()) < 0){
+        printf("ERROR: Forked failed.\n\n");
+        exit(1);
+    }
+    else if (p == 0){
+        if((execvp(cmd, args) < 0)){
+            printf("ERROR: Execvp failed.\n\n");
+        }
+        head->pid = p;
+        strcpy(head->command, *args);
+        head->next = NULL;
+    }
+    else{
+        bg_list_append(p, args);
+        while(wait(&status) == 0)
+            ;
+    }
+
+}
+
+
+void process_bg(char *args[10]) {
+    printf("Process bg...\n");
+    char *new_args[10];
+    printf("before for loop");
+    for(int i = 1; i < 10; i++){
+        new_args[i-1] = args[i];
+    }
+    new_args[9] = NULL;
+    execute_bg_program(new_args[0], new_args);
+}
+
+/*
+void execute_bg_program(char* cmd, char *args[10]){
+    pid_t pid;
+    int status;
+
+    if((pid = fork()) < 0){
+        printf("ERROR: Forked failed.\n\n");
+        exit(1);
+    }
+    else if (pid == 0){
+        if((execvp(cmd, args) < 0)){
+            printf("ERROR: Execvp failed.\n\n");
+        }
+    }
+    else{
+        if((execvp(cmd, args) == 0)){
+
+        }
+        else{
+
+        }
+    }
+
+}
+*/
+
+int main() {
+    printf("MAIN\n");
+    int bailout = 0;
+    head = NULL;
+    while (!bailout) {
+        char prompt[1000] = "";
+        char *reply = readline(create_prompt(prompt));
+        printf("reply\n");
+        char *argsv[10];
+        int i = 0;
+        char *token = strtok(reply, " ");
+        if (token == NULL){
+            continue;
+        }
+        for (i = 0; token != NULL; i++) {
+            argsv[i] = token;
+            token = strtok(NULL, " ");
+        }
+
+        argsv[i] = NULL;
+        int end_of_argsv = i;
+
+        if (!strcmp(argsv[0], "quit")) {
+            bailout = 1;
+        } else if (!strcmp(argsv[0], "cd")) {
+            process_cd(argsv);
+        } else if (!strcmp(argsv[0], "bg")) {
+            printf("First argument is bg\n");
+            process_bg(argsv);
+        } else if (!strcmp(argsv[0], "bglist")) {
+            print_bglist();
+        } else {
+            execute_program(argsv[0], argsv);
+        }
+
+            free(reply);
+    }
+        printf("Quitting...\n");
+        exit(0);
+}
+
+
